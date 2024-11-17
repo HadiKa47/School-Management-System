099bc09fe102d1b04a8a08f00a81faa7c15bb49b
HadiKa47 <Hade.Kary24@gmail.com>
2024-11-17 13:12:58 +0200

    Admin Dashboard Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,18 @@
+import UserCard from "@/components/UserCard";
+
 export default function AdminPage() {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+    </div>
+  );
 }
