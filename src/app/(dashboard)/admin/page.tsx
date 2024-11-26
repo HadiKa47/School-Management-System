5866bc99c48ae2494ba53fc4a52ec160bd3eb651
HadiKa47 <Hade.Kary24@gmail.com>
2024-11-26 11:28:16 +0200

    React Calendar Events Component Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
@@ -30,6 +31,10 @@ export default function AdminPage() {
         <div className="w-full h-[500px]">
           <FinanceChart />
         </div>
+      </div>
+      {/* Right */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
       </div>
     </div>
   );
